2de6ea5249c98a07922ad8528c4e565bdadd08a8
phuongyajin_FPTU <phuongyajin@gmail.com>
2021-11-28 10:43:58 +0700

    make navbar, add menu image, remove jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
